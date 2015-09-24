af34239aad8b9cb02584384510aaf0d0f49820a9
rperezde11 <raulp_prat@hotmail.com>
2015-09-24 14:42:09 +0200

    added scripts structure folders & begun the mainmenulogic script

---
+++ March Death/Assets/Scripts/Storage/MainMenuLogic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuLogic : MonoBehaviour {
+
+	/* Each one of these bool variables corresponds to the 
+	 * options of the main menu. */
+	public bool bStart = false;
+	public bool bTutorial = false;
+	public bool bQuit = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
