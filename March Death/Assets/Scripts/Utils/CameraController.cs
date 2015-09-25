77fb99240c790afec33c01be7bd802d7511dd20b
4nc3str4l <cmuri_10@hotmail.com>
2015-09-25 13:09:55 +0200

    Now camera is able to be mooved with arrow keys and has methods to look at certain position

--- March Death/Assets/Scripts/Utils/CameraController.cs
+++ March Death/Assets/Scripts/Utils/CameraController.cs
@@ -1,19 +1,36 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Attach this script to the main camera and it will be able to be controlled like an Isometric Camera.
+/// </summary>
 [RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour {
 
-    private GameObject followingGameObject;
+    private Vector3 cameraOffset;
+    private  GameObject followingGameObject;
+    private GameObject cameraContainer;
     private bool isManualControlEnabled;
     private float speed;
 
-	void Start () {
+    void Start()
+    {
+        setupCamera();
         this.isManualControlEnabled = true;
         this.speed = 5f;
-	}
-	
-	void Update () {
-	}
+    }
+
+    void Update()
+    {
+        if (this.followingGameObject != null)
+        {
+            this.lookGameObject(followingGameObject);
+        }
+
+        if (this.isManualControlEnabled)
+        {
+            handlePlayerInput();
+        }
+    }
 
     /// <summary>
     ///  Make the camera look at certain position.
@@ -21,16 +38,21 @@ public class CameraController : MonoBehaviour {
     /// <param name="target"></param>
     public void lookAtPoint(Vector3 target)
     {
+        this.followingGameObject = null;
+        Vector3 newCameraPos = target + cameraOffset;
+        this.cameraContainer.transform.position = newCameraPos;
     }
 
     /// <summary>
-    /// Make the camera look at certain game object.
+    /// Make the camera look to certain game object.
     /// </summary>
     /// <param name="target"></param>
     public void lookGameObject(GameObject target)
-    {     
+    {
+        Vector3 newCameraPos = target.transform.position + cameraOffset;
+        this.cameraContainer.transform.position = newCameraPos;
     }
-    
+
     /// <summary>
     /// Make the camera follow a game object.
     /// </summary>
@@ -82,7 +104,7 @@ public class CameraController : MonoBehaviour {
     /// </summary>
     /// <param name="target">The desired target that you want to reach</param>
     /// <param name="speed">Optional if not provided the speed will be default camera speed</param>
-    public void smothTravelToTarget(GameObject target, float speed = -1)
+    public void smoothTravelToTarget(GameObject target, float speed = -1)
     {
     }
 
@@ -95,6 +117,42 @@ public class CameraController : MonoBehaviour {
     /// <param name="speed">Optional if not provided the speed will be default camera speed</param>
     public void smoothTravelToPosition(Vector3 position, float speed = -1)
     {
+    }
+
+    private void handlePlayerInput()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            this.cameraContainer.transform.Translate(Vector3.forward * Time.deltaTime * this.speed);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.cameraContainer.transform.Translate(Vector3.left * Time.deltaTime * this.speed);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            this.cameraContainer.transform.Translate(Vector3.back * Time.deltaTime * this.speed);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            this.cameraContainer.transform.Translate(Vector3.right * Time.deltaTime * this.speed);
+        }
+    }
+
+    private void setupCamera()
+    {
+        this.cameraOffset = new Vector3(-10f, 13.4f, -10f);
+        Vector3 desiredCameraPosition = new Vector3(this.transform.position.x, this.cameraOffset.y, this.transform.position.z);
+        this.cameraContainer = new GameObject("Camera");
+        this.transform.localEulerAngles = new Vector3(45f, 0f, 0f);
+        this.gameObject.transform.parent = this.cameraContainer.transform;
+        this.transform.localPosition = Vector3.zero;
+        this.transform.localEulerAngles = new Vector3(this.transform.localEulerAngles.x, 0, this.transform.localEulerAngles.z);
+        this.cameraContainer.transform.position = desiredCameraPosition;
+        this.cameraContainer.transform.Rotate(Vector3.up, 45f);
     }
 
 }
