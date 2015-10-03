03362b0a9ff10025e28b94f5eb03217573430474
aviaplana <e1428076@student.tuwien.ac.at>
2015-10-03 19:22:32 +0200

    Added building classes

--- March Death/Assets/Scripts/Entities/Building.cs
+++ March Death/Assets/Scripts/Entities/Building.cs
@@ -1,8 +1,38 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using UnityEngine;
+using Storage;
 
-public sealed class Building
+
+/// <summary>
+/// Building base class. Extends actor (which in turn extends MonoBehaviour) to
+/// handle basic API operations
+/// </summary>
+public class Building : Utils.Actor<Building.Actions>, IGameEntity
 {
+    public enum Actions { DAMAGED, DESTROYED };
+
+    public Building() { }
+
+
+    /// <summary>
+    /// Called every fixed physics frame
+    /// </summary>
+    void FixedUpdate()
+    {
+    }
+
+    /// <summary>    
+    /// Returns NULL as this cannot be converted to Unit
+	/// </summary>
+    /// <returns>Object casted to Unit</returns>
+    public Unit toUnit() { return null;  }
+
+    /// <summary>
+    /// Casts this IGameEntity to Unity (pointless if already building)
+    /// </summary>
+    /// <returns>Always null</returns>
+    public Building toBuilding() { return this; }
+
 }
