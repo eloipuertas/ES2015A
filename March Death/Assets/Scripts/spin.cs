cbb94067a58d0084acc9cf566f11bf4a43dc4210
rperezde11 <raulp_prat@hotmail.com>
2015-10-03 16:35:37 +0200

    added logic of pause menu to the hud

---
+++ March Death/Assets/Scripts/spin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0f,0f,600f * Time.deltaTime));
+	}
+}
