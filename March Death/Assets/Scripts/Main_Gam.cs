8ef13ce9a56b3b14423b669b9b7078484bd1a748
rperezde11 <raulp_prat@hotmail.com>
2015-10-03 19:48:55 +0200

    Linked minimap w/ hud prefab

--- March Death/Assets/Scripts/Main_Gam.cs
+++ March Death/Assets/Scripts/Main_Gam.cs
@@ -7,6 +7,6 @@ public class Main_Gam : MonoBehaviour {
 	void Start () {
 		Instantiate((GameObject)Resources.Load ("HUD")).name = "HUD";
 		Instantiate((GameObject)Resources.Load ("HUD_EventSystem")).name = "HUD_EventSystem";
-	}
+    }
 
 }
