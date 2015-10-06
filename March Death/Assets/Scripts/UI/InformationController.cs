1b073ba33e8d5b3f298eeab05f25e399e7c259e4
daniestrella1 <daniestrella1@gmail.com>
2015-10-06 22:38:36 +0200

    prepare informationController for sprint 2

--- March Death/Assets/Scripts/UI/InformationController.cs
+++ March Death/Assets/Scripts/UI/InformationController.cs
@@ -1,37 +1,68 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using Storage;
 
 public class InformationController : MonoBehaviour {
 
+	private Text txtActorName;
+	private Text txtActorRace;
+	private Text txtActorHealth;
+	private Image imgActorDetail;
+	private Slider sliderActorHealth;
+	
 	// Use this for initialization
 	void Start () {
 
-		GameObject hud = GameObject.Find ("HUD");	
-		if (hud != null) {
-			Transform information = hud.transform.FindChild ("Information");
-			if (information != null) {
-				Transform txtActorName = information.transform.FindChild("ActorName");
-				Transform txtActorRace = information.transform.FindChild ("ActorRace");
-				Transform txtActorHealth = information.transform.FindChild("ActorHealth");
-				Transform txtActorImage = information.transform.FindChild ("ActorImage");
-				Transform sliderActorHealth = information.transform.FindChild ("ActorHealthSlider");
-				
-				txtActorName.gameObject.GetComponent<Text>().enabled = false;
-				txtActorRace.gameObject.GetComponent<Text>().enabled = false;
-				txtActorHealth.gameObject.GetComponent<Text>().enabled = false;
-				txtActorImage.gameObject.GetComponent<Image>().enabled = false;
-				sliderActorHealth.gameObject.GetComponent<Slider>().enabled = false;
-				sliderActorHealth.gameObject.GetComponent<Slider>().value = 0;
-				
-				Transform sliderBackground = sliderActorHealth.transform.FindChild ("Background");
-				sliderBackground.GetComponent<Image>().enabled = false;
-			}
-		}
+		//Init menu components
+		Transform information = GameObject.Find ("HUD").transform.FindChild ("Information");
+		txtActorName = information.transform.FindChild("ActorName").gameObject.GetComponent<Text>();
+		txtActorRace = information.transform.FindChild ("ActorRace").gameObject.GetComponent<Text>();
+		txtActorHealth = information.transform.FindChild("ActorHealth").gameObject.GetComponent<Text>();
+		imgActorDetail = information.transform.FindChild ("ActorImage").gameObject.GetComponent<Image>();
+		sliderActorHealth = information.transform.FindChild ("ActorHealthSlider").gameObject.GetComponent<Slider>();
+
+		//Default is hidden
+		hideInformation ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
 	}
+
+	private void dosomething() 
+	{
+		IGameEntity entity = gameObject.GetComponent<IGameEntity>();
+		Unit unit = entity.toUnit ();
+		unit.register(Unit.Actions.DAMAGED, onUnitDamaged);
+		unit.register(Unit.Actions.DAMAGED, onUnitDied);
+	}
+
+	private void hideInformation() 
+	{	
+		txtActorName.enabled = false;
+		txtActorRace.enabled = false;
+		txtActorHealth.enabled = false;
+		imgActorDetail.enabled = false;
+		sliderActorHealth.enabled = false;
+		sliderActorHealth.value = 0;
+		
+		Transform sliderBackground = sliderActorHealth.transform.FindChild ("Background");
+		sliderBackground.GetComponent<Image>().enabled = false;
+
+	}
+
+	public void onUnitDamaged(GameObject gameObject)
+	{
+		IGameEntity entity = gameObject.GetComponent<IGameEntity> ();
+		sliderActorHealth.value = entity.healthPercentage;
+	}
+
+	public void onUnitDied(GameObject gameObject)
+	{
+		hideInformation ();
+	}
+		
 }
