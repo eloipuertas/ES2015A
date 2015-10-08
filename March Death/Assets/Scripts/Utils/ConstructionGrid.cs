3874d4a25fdaf86413e9d761ff5c4f27756bf164
4nc3str4l <cmuri_10@hotmail.com>
2015-10-08 13:27:06 +0200

    Basic methods of the construction API (public ones will not be modified).

--- March Death/Assets/Scripts/Utils/ConstructionGrid.cs
+++ March Death/Assets/Scripts/Utils/ConstructionGrid.cs
@@ -3,13 +3,59 @@ using System.Collections;
 
 public class ConstructionGrid : MonoBehaviour {
 
-	// Use this for initialization
+    private Vector2 dimensions;
+    private ArrayList reservedPositions;
+
 	void Start () {
-	
+        dimensions = new Vector2(100f, 100f);
+        reservedPositions = new ArrayList();    	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    /// <summary>
+    /// Returns the center of a row of the grid where the building will be placed
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns></returns>
+	public Vector3 discretizeMapCoords(Vector3 position)
+    {
+        return Vector3.zero;
+    }
+
+    /// <summary>
+    /// Detects if a position is flat enougth to construct in passed row
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns></returns>
+    private bool isFlatEnoughtForConstruction(Vector3 discretizedPosition)
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Used to ask the construction grid if a discretized position is able to construct in.
+    /// </summary>
+    /// <param name="discretizedPosition"></param>
+    /// <returns></returns>
+    private bool isNewPositionAbleForConstrucction(Vector3 discretizedPosition)
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Sets the new grid dimensions
+    /// </summary>
+    /// <param name="newDimensions"></param>
+    public void setNewGridDimensions(Vector2 newDimensions)
+    {
+        dimensions = newDimensions;
+    }
+
+    /// <summary>
+    /// Used to reserve a place to prevent others to construct in
+    /// </summary>
+    /// <param name="discretizedPosition"></param>
+    public void reservePosition(Vector3 buildingDiscretizedPosition)
+    {
+        reservedPositions.Add(buildingDiscretizedPosition);
+    }
 }
