0231c594dc201c11ab56e198d515faf08cc4c0ba
4nc3str4l <cmuri_10@hotmail.com>
2015-10-08 13:29:58 +0200

    Methods for the construcction API.

--- March Death/Assets/Scripts/Utils/ConstructionGrid.cs
+++ March Death/Assets/Scripts/Utils/ConstructionGrid.cs
@@ -22,6 +22,33 @@ public class ConstructionGrid : MonoBehaviour {
     }
 
     /// <summary>
+    /// Sets the new grid dimensions
+    /// </summary>
+    /// <param name="newDimensions"></param>
+    public void setNewGridDimensions(Vector2 newDimensions)
+    {
+        dimensions = newDimensions;
+    }
+
+    /// <summary>
+    /// Used to reserve a place to prevent others to construct in
+    /// </summary>
+    /// <param name="discretizedPosition"></param>
+    public void reservePosition(Vector3 buildingDiscretizedPosition)
+    {
+        reservedPositions.Add(buildingDiscretizedPosition);
+    }
+
+    /// <summary>
+    /// Libreates the current discretized position
+    /// </summary>
+    /// <param name="discretizedPosition"></param>
+    public void liberatePosition(Vector3 discretizedPosition)
+    {
+        //liberate free this position
+    }
+
+    /// <summary>
     /// Detects if a position is flat enougth to construct in passed row
     /// </summary>
     /// <param name="position"></param>
@@ -39,23 +66,5 @@ public class ConstructionGrid : MonoBehaviour {
     private bool isNewPositionAbleForConstrucction(Vector3 discretizedPosition)
     {
         return true;
-    }
-
-    /// <summary>
-    /// Sets the new grid dimensions
-    /// </summary>
-    /// <param name="newDimensions"></param>
-    public void setNewGridDimensions(Vector2 newDimensions)
-    {
-        dimensions = newDimensions;
-    }
-
-    /// <summary>
-    /// Used to reserve a place to prevent others to construct in
-    /// </summary>
-    /// <param name="discretizedPosition"></param>
-    public void reservePosition(Vector3 buildingDiscretizedPosition)
-    {
-        reservedPositions.Add(buildingDiscretizedPosition);
     }
 }
