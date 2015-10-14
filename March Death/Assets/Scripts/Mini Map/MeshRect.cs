ae0802f0899ae6a01bcd996e7e2ad625674723ca
rperezde11 <raulp_prat@hotmail.com>
2015-10-14 05:45:12 +0200

    implemented change on maincam when clicking on minimap and added rectangle to it

---
+++ March Death/Assets/Scripts/Mini Map/MeshRect.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshRect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        createRectangle();
+
+	}
+
+    private void createRectangle()
+    {
+        MeshFilter mf = GetComponent<MeshFilter>();
+        Mesh mesh = new Mesh();
+        mf.mesh = mesh;
+
+        float height = 500f;
+        float width = 500f;
+
+        // Vertices
+        Vector3[] vertices = new Vector3[4]
+        {
+            new Vector3(-width/2,0,-height/2),
+            new Vector3(width/2, 0, -height/2),
+            new Vector3(-width/2, 0, height/2),
+             new Vector3(width/2, 0, height/2)
+        };
+
+        // Triangles
+        int[] triangles = new int[6]
+        {
+            0,2,1,
+            2,3,1
+        };
+
+        // Normals
+        Vector3[] normals = new Vector3[4]
+        {
+            Vector3.up,
+            Vector3.up,
+            Vector3.up,
+            Vector3.up
+        };
+
+        // uvs
+        Vector2[] uvs = new Vector2[4]
+        {
+            new Vector2(0,0),
+            new Vector2(1,0),
+            new Vector2(0,1),
+            new Vector2(1,1),
+        };
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.normals = normals;
+        mesh.uv = uvs;
+
+    }
+}
--- March Death/Assets/Scripts/Mini Map/MiniMapController.cs
+++ March Death/Assets/Scripts/Mini Map/MiniMapController.cs
@@ -5,16 +5,27 @@ public class MiniMapController : MonoBehaviour
 {
 
     private Camera _camera;
+    private Camera mainCam;
+    
+    // Minimap Rectangle
+    GameObject quad;
+    private float main_zoom;
+    private Vector3 act_pos;
+    Vector3 cameraOffset;
+
     // Use this for initialization
     void Start()
     {
-
         this.GetComponent<AudioListener>().enabled = false;
 
         _camera = this.GetComponent<Camera>();
         _camera.orthographic = true;
         //Assign camera viewport
         _camera.rect = this.recalcViewport();
+
+        mainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+        initializeMinimapRect();
 
         // moves camera to show the whole map
         if (Terrain.activeTerrain)
@@ -29,15 +40,95 @@ public class MiniMapController : MonoBehaviour
 
     }
 
+    private void initializeMinimapRect()
+    {
+        main_zoom = mainCam.orthographicSize;
+        act_pos = mainCam.transform.position;
+        cameraOffset = new Vector3(-252.8f, 0f, -252.8f);
+        createRectangle();
+    }
+
     // Update is called once per frame
+    /// <summary>
+    /// 
+    /// </summary>
     void Update()
     {
+        // GetMouseButton(0) if we want to move the camera by clicking & dragging around the minimap
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            RaycastHit hit;
 
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+            if (isInside(pos, _camera.rect)) {
+                Vector3 ground = new Vector3();
+                if (Physics.Raycast(_camera.ScreenToWorldPoint(Input.mousePosition), -Vector3.up, out hit)) 
+                    ground = hit.point;          
+                if(!ground.Equals(new Vector3()))
+                    mainCam.GetComponent<CameraController>().lookAtPoint(ground);
+                updateRect();
+            }
+        }
+        updateRect();
+
+    }
+
+    /// <summary>
+    /// Creates and sets up the rectangle to show the view on the minimap.
+    /// </summary>
+    private void createRectangle()
+    {
+        Vector3 v = mainCam.transform.position;
+
+        quad = new GameObject();
+        quad.AddComponent<MeshFilter>();
+        quad.AddComponent<MeshRenderer>();
+        MeshRenderer mr = quad.GetComponent<MeshRenderer>();
+        Material m = new Material(Shader.Find("Transparent/Diffuse"));
+        m.color = new Color(1f, 0f, 0f, 0.35f);
+        mr.material = m;
+        quad.transform.position = v - cameraOffset;
+        quad.AddComponent<MeshRect>();
+        quad.layer = 8;
+    }
+
+    /// <summary>
+    /// Updates the parameters of the Rectangle of the minimap.
+    /// </summary>
+    private void updateRect()
+    {
+        if (main_zoom != mainCam.orthographicSize)
+        {
+            float diff = (mainCam.orthographicSize - main_zoom)/ 80;
+            quad.transform.localScale += new Vector3(diff, diff, diff);
+            main_zoom = mainCam.orthographicSize;
+        }
+        if (!act_pos.Equals(mainCam.transform.position))
+        {
+            Vector3 vDiff = mainCam.transform.position - act_pos;
+            quad.transform.Translate(vDiff);
+            act_pos = mainCam.transform.position;
+        }
+    }
+
+    /// <summary>
+    /// Checks if a coordinate is inside a certain rectangle
+    /// </summary>
+    /// <param name="coord"> Vector3 that represent the coordinate.</param>
+    /// <param name="r">Rectangle that represents the box.</param>
+    /// <returns></returns>
+    private bool isInside(Vector3 coord, Rect r)
+    {
+        if (coord.x >= r.xMin && coord.x <= r.xMax)
+        {
+            return (coord.y >= r.yMin && coord.y <= r.yMax);
+        }
+        else { return false; }
     }
 
     private Rect recalcViewport()
     {
-
 
         float viewPortPosX = 0.007f;
         float viewPortPosY = 0.007f;
@@ -52,4 +143,6 @@ public class MiniMapController : MonoBehaviour
         return new Rect(viewPortPosX, viewPortPosY, viewPortW, viewPortH);
 
     }
+
+
 }
