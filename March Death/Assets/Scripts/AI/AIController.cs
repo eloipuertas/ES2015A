c142e1d544e4162a4024f22e2295f088cd5eeb80
MSoler <msolerpl10@alumnes.ub.edu>
2015-10-16 02:55:51 +0200

    [AI] Basic structure

---
+++ March Death/Assets/Scripts/AI/AIController.cs
@@ -0,0 +1,68 @@
+﻿using Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// Class tasked with deciding what the enemy will do.
+/// </summary>
+namespace Assets.Scripts.AI 
+{
+    public class AIController : MonoBehaviour
+    {
+        public MacroManager Macro { get; set; }
+        public MicroManager Micro { get; set; }
+
+        /// <summary>
+        /// Will be used to calc the lvl of the AI
+        /// </summary>
+        int difficultyLvl;
+
+        List<AIModule> modules;
+        //TODO: change this when decided about what do we really need to keep about buildings 
+        public List<MonoBehaviour> OwnBuildings { get; set; }
+        public List<MonoBehaviour> EnemyBuildings { get; set; }
+        /// <summary>
+        /// Just a basic way to keep track of what the enemy has more
+        /// </summary>
+        public Dictionary<UnitTypes,int> UnitsFound { get; set; }
+
+        public List<Unit> Army { get; set; }
+        public List<Unit> Workers { get; set; }
+        float timer; //It's not going to overflow this millennium
+        public AIController()
+        {
+            Macro = new MacroManager(this);
+            Micro = new MicroManager(this);
+            modules = new List<AIModule>();
+            Army = new List<Unit>();
+            Workers = new List<Unit>();
+
+            modules.Add(new AIModule(Macro.MacroHigh, 30f));
+            modules.Add(new AIModule(Macro.MacroLow, 1f));
+            modules.Add(new AIModule(Micro.Micro, 0.3f));
+            timer = 0;
+        }
+        void Update()
+        {
+            timer += Time.deltaTime;
+            foreach (AIModule m in modules)
+                if (Math.Round(timer % m.period) == 0)
+                    m.Callback();
+        }
+    }
+    struct AIModule
+    {
+        public AIModule(Action Callback, float period) {
+            this.Callback = Callback;
+            this.period = period;
+        }
+        public Action Callback;
+        /// <summary>
+        /// Time in seconds between each call to Callback
+        /// </summary>
+        public float period;
+    }
+}
