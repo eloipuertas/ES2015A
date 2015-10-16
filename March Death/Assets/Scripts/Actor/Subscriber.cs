73159cdc478527a281f9492fcc937544ed8c1ea2
Blipi <gpascualg93@gmail.com>
2015-10-16 13:16:22 +0200

    Allow to use a selector/condition on Subscriber(s)

--- March Death/Assets/Scripts/Actor/Subscriber.cs
+++ March Death/Assets/Scripts/Actor/Subscriber.cs
@@ -7,7 +7,10 @@ namespace Utils
     public sealed class Subscriber<T, S> : Singleton<Subscriber<T, S>> where T : struct, IConvertible where S : class
     {
 
-		private Subscriber()
+        private Dictionary<T, List<Action<Object>>> callbacks = new Dictionary<T, List<Action<Object>>>();
+        private Dictionary<T, Dictionary<Action<Object>, Func<UnityEngine.GameObject, bool>>> conditions = new Dictionary<T, Dictionary<Action<Object>, Func<UnityEngine.GameObject, bool>>>();
+
+        private Subscriber()
         {
             if (!typeof(T).IsEnum)
             {
@@ -17,20 +20,30 @@ namespace Utils
             foreach (T action in Enum.GetValues(typeof(T)))
             {
                 callbacks.Add(action, new List<Action<Object>>());
+                conditions.Add(action, new Dictionary<Action<Object>, Func<UnityEngine.GameObject, bool>>());
             }
         }
 
-        private Dictionary<T, List<Action<Object>>> callbacks = new Dictionary<T, List<Action<Object>>>();
-
         public void registerForAll(T action, Action<Object> func)
         {
+            registerForAll(action, func, x => true);
+        }
+
+        public void registerForAll(T action, Action<Object> func, Func<UnityEngine.GameObject, bool> condition)
+        {
             callbacks[action].Add(func);
+            conditions[action].Add(func, condition);
 
             // Find all already existing gameobjects of this type
             UnityEngine.Object[] alreadyExistingActors = UnityEngine.Object.FindObjectsOfType(typeof(SubscribableActor<T,S>));
             foreach (UnityEngine.Object obj in alreadyExistingActors)
             {
-                ((SubscribableActor<T, S>)obj).register(action, func);
+                var actor = (SubscribableActor<T, S>)obj;
+
+                if (condition.Invoke(actor.gameObject))
+                {
+                    actor.register(action, func);
+                }
             }
         }
 
@@ -52,7 +65,10 @@ namespace Utils
             {
                 foreach (Action<Object> action in entry.Value.ToList())
                 {
-                    actor.register(entry.Key, action);
+                    if (conditions[entry.Key][action].Invoke(actor.gameObject))
+                    {
+                        actor.register(entry.Key, action);
+                    }
                 }
             }
         }
