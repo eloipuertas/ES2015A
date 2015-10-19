b4726d792667a3ee427fa9a06b996e6fc1f2e7a9
c30zD <c30zD.x@gmail.com>
2015-10-19 02:33:20 +0200

    Rename prefabs. Now Men and Elves have both a distinct HUD and pause menu, each in a different prefab.
    Adapt logic to changes (MainMenuButtonLogic.cs)
    
    Adapt game data to changes (GameInformation.cs)

--- March Death/Assets/Scripts/Gameplay/GameInformation.cs
+++ March Death/Assets/Scripts/Gameplay/GameInformation.cs
@@ -4,17 +4,19 @@ using Storage;
 
 public class GameInformation : MonoBehaviour {
 
-	private Races playerRace;
+    private Races playerRace;
+
+    private static string pauseMenuPrefab;
 
     // Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
 
     void Awake()
     {
@@ -27,40 +29,63 @@ public class GameInformation : MonoBehaviour {
         {
         case Races.ELVES:
             LoadElfHUD();;
-			break;
-		case Races.MEN:
+            break;
+        case Races.MEN:
             LoadHumanHUD();
-			break;
+            break;
         }
-	}
+    }
 
-	// TODO Modify to use the actual HUD for each civilization
+    // TODO Modify to use the actual HUD for each civilization
 
-	private static void LoadElfHUD()
-	{
-		Instantiate((GameObject)Resources.Load ("HUD")).name = "HUD";
-		Instantiate((GameObject)Resources.Load ("HUD_EventSystem")).name = "HUD_EventSystem";
-	}
+    private static void LoadElfHUD()
+    {
+        Instantiate((GameObject)Resources.Load ("HUD-Elf")).name = "HUD";
+        Instantiate((GameObject)Resources.Load ("HUD_EventSystem")).name = "HUD_EventSystem";
+    }
 
-	private static void LoadHumanHUD()
-	{
-		Instantiate((GameObject)Resources.Load ("Human_HUD")).name = "HUD";
-		//Application.LoadLevelAdditive("globalHUDHuman");
-		Instantiate((GameObject)Resources.Load ("HUD_EventSystem")).name = "HUD_EventSystem";
-	}
+    private static void LoadHumanHUD()
+    {
+        Instantiate((GameObject)Resources.Load ("HUD-Human")).name = "HUD";
+        //Application.LoadLevelAdditive("globalHUDHuman");
+        Instantiate((GameObject)Resources.Load ("HUD_EventSystem")).name = "HUD_EventSystem";
+    }
 
-	public void SetPlayerRace(int race)
-	{
-		playerRace = (Races) race;
-	}
+    public void SetPlayerRace(int race)
+    {
+        playerRace = (Races) race;
+        switch(playerRace)
+        {
+            case Races.ELVES:
+                pauseMenuPrefab = "PauseMenu-Elf";
+                break;
+            case Races.MEN:
+                pauseMenuPrefab = "PauseMenu-Human";
+                break;
+        }
+    }
 
     public void SetPlayerRace(Races race)
     {
         playerRace = race;
+        switch(playerRace)
+        {
+            case Races.ELVES:
+                pauseMenuPrefab = "PauseMenu-Elf";
+                break;
+            case Races.MEN:
+                pauseMenuPrefab = "PauseMenu-Human";
+                break;
+        }
     }
 
-	public Races GetPlayerRace()
-	{
-		return playerRace;
+    public Races GetPlayerRace()
+    {
+        return playerRace;
+    }
+
+    public string GetPauseMenuPrefabPath()
+    {
+        return pauseMenuPrefab;
     }
 }
