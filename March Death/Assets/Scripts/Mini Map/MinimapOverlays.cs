c334bb98cd294074c370fe80cf8637a1bc780de3
Blipi <gpascualg93@gmail.com>
2015-10-27 02:15:00 +0100

    Change that should not have been commited

--- March Death/Assets/Scripts/Mini Map/MinimapOverlays.cs
+++ March Death/Assets/Scripts/Mini Map/MinimapOverlays.cs
@@ -46,7 +46,6 @@ public static class MinimapOverlays
     public static Rect CalculateBoxFromCntr(Vector3 cntr, Camera cam, int pix)
     {
         Rect box = new Rect();
-        return box;
 
         Vector3 actor_cntr = cam.WorldToViewportPoint(cntr);
         actor_cntr = cam.ViewportToScreenPoint(actor_cntr);
