0d0b7688f8268434f2d710cf86ebab335dc10101
gpascualg <gpascualg93@gmail.com>
2015-10-29 14:52:07 +0100

    Try hardcoding date :/

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,19 +1,16 @@
-# Adapted to use HEAD rather than the new commit ref
-get_file_rev() {
-    git rev-list -n 1 HEAD "$1"
+#!/bin/sh
+
+gittouch() {
+    NEWDATE="$(git log -1 --format=%ci "$1")"
+    sudo -E date --set="$NEWDATE"
+    touch -ch -d "$NEWDATE" "$1"
 }
 
-# Same as Giel's answer above
-update_file_timestamp() {
-    file_time=`git show --pretty=format:%ai --abbrev-commit "$(get_file_rev "$1")" | head -n 1`
-    sudo touch -d "$file_time" "$1"
-}
+OLDDATE=$(date)
 
-# Loop through and fix timestamps on all files in our CDN directory
-old_ifs=$IFS
-IFS=$'\n' # Support files with spaces in them
-for file in $(git ls-files | grep "$cdn_dir")
-do
-    update_file_timestamp "${file}"
-done
-IFS=$old_ifs
+git ls-files |
+    while IFS= read -r file; do
+        gittouch "$file"
+    done
+
+sudo -E date --set="$OLDDATE"
