9d088277a36c09944b42084036288538ad5c0ac4
hermetico <yojmarinv@gmail.com>
2015-11-13 18:35:13 +0100

    Silly audio pooling with AudioPool. Added to Sounds manager to control sounds when entities are selected and when units perform an action.

--- March Death/Assets/Scripts/Managers/SoundsManager.cs
+++ March Death/Assets/Scripts/Managers/SoundsManager.cs
@@ -7,70 +7,78 @@ namespace Managers
     public class SoundsManager: MonoBehaviour
     {
 
-        public AudioSource efxSource;
-        public AudioClip selectionSound;
-        public AudioClip actionSound;
+        private AudioPool selectionSoundPool;
+        private AudioPool actionSoundPool;
+        private AudioClip[] selectionSound;
+        private AudioClip[] actionSound;
 
 
         void Start()
         {
-            // extraido de la wiki de unity
-            //if (Instance != this) Destroy(gameObject);
-            //DontDestroyOnLoad(gameObject);
+            Setup();
+            fakeSetup();
 
-            //if(information == null) information = GameObject.Find("GameInformationObject").GetComponent<GameInformation>();
-            //Register to selectable actions
-            //selectionSound = (AudioClip)Resources.Load("Sounds/test-fx/civil-selected");
-            //actionSound = (AudioClip)Resources.Load("Sounds/test-fx/civil-action");
-            Subscriber<SelectionManager.Actions, SelectionManager>.get.registerForAll(SelectionManager.Actions.SELECT, onUnitSelected);
+        }
+
+        /// <summary>
+        /// Setups the SoundsManager
+        /// </summary>
+        void Setup()
+        {
+            selectionSoundPool = new AudioPool(this.gameObject, 3);
+            actionSoundPool = new AudioPool(this.gameObject, 3);
+
+            // suscribes the audio managet to main events 
+            Subscriber<SelectionManager.Actions, SelectionManager>.get.registerForAll(SelectionManager.Actions.SELECT, onEntitytSelected);
             Subscriber<SelectionManager.Actions, SelectionManager>.get.registerForAll(SelectionManager.Actions.ATTACK, onUnitAction);
             Subscriber<SelectionManager.Actions, SelectionManager>.get.registerForAll(SelectionManager.Actions.MOVE, onUnitAction);
         }
 
-
-
-        //Used to play single sound clips.
-        public void PlaySingle(AudioClip clip)
+        /// <summary>
+        /// Adds test sounds
+        /// </summary>
+        void fakeSetup()
         {
-            //Set the clip of our efxSource audio source to the clip passed in as a parameter.
-            efxSource.clip = clip;
+            selectionSound = new AudioClip[3];
+            actionSound = new AudioClip[3];
 
-            //Play the clip.
-            efxSource.Play();
-        }
+            //Register to selectable actions
+            selectionSound[0] = (AudioClip)Resources.Load("Sounds/test-fx/civil-selected");
+            selectionSound[1] = (AudioClip)Resources.Load("Sounds/test-fx/hero-selected");
+            selectionSound[2] = (AudioClip)Resources.Load("Sounds/test-fx/hero-selected-2");
 
-        //Used to play single sound clips.
-        public void PlaySingle()
-        {
-            //Set the clip of our efxSource audio source to the clip passed in as a parameter.
-            efxSource.clip = selectionSound;
-
-            //Play the clip.
-            efxSource.Play();
+            actionSound[0] = (AudioClip)Resources.Load("Sounds/test-fx/civil-action");
+            actionSound[1] = (AudioClip)Resources.Load("Sounds/test-fx/hero-action");
+            actionSound[2] = (AudioClip)Resources.Load("Sounds/test-fx/hero-action-2");
         }
 
 
-        public void onUnitSelected(System.Object obj)
+        /// <summary>
+        /// function to be triggered when an entity is selected
+        /// </summary>
+        /// <param name="obj"></param>
+        public void onEntitytSelected(System.Object obj)
         {
-            //Set the clip of our efxSource audio source to the clip passed in as a parameter.
-            efxSource.clip = selectionSound;
-
-            //Play the clip.
-            efxSource.Play();
+            selectionSoundPool.Play(selectionSound[RandomChoice()]);
         }
 
+        /// <summary>
+        /// function to be triggered when a unit performs an action
+        /// </summary>
+        /// <param name="obj"></param>
         public void onUnitAction(System.Object obj)
         {
-            //Set the clip of our efxSource audio source to the clip passed in as a parameter.
-            efxSource.clip = actionSound;
-
-            //Play the clip.
-            efxSource.Play();
+            actionSoundPool.Play(actionSound[RandomChoice()]);
         }
-        // Update is called once per frame
-        void Update()
+
+        /// <summary>
+        /// Randomizes test sounds
+        /// </summary>
+        /// <returns></returns>
+        private int RandomChoice()
         {
-
+            return Random.Range(0, 3);
         }
+
     }
 }
