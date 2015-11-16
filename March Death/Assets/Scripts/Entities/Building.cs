30be73c53414f09668a8fd6afa5ad5ac2c630c54
aviaplana <pichi.pride@gmail.com>
2015-11-16 13:28:24 +0100

    Add resources instead of substracting when a unit is cancelled from the creation queue

--- March Death/Assets/Scripts/Entities/Building.cs
+++ March Death/Assets/Scripts/Entities/Building.cs
@@ -232,9 +232,9 @@ public abstract class Building<T> : GameEntity<T> where T : struct, IConvertible
 			IGameEntity entity = gameObject.GetComponent<IGameEntity> ();
 			UnitInfo unitInfo = Info.get.of (info.race, (UnitTypes) _creationQueue.Dequeue ());
 
-			Player.getOwner (entity).resources.SubstractAmount (WorldResources.Type.WOOD, unitInfo.resources.wood);
-			Player.getOwner (entity).resources.SubstractAmount (WorldResources.Type.METAL, unitInfo.resources.metal);
-			Player.getOwner (entity).resources.SubstractAmount (WorldResources.Type.FOOD, unitInfo.resources.food);
+			Player.getOwner (entity).resources.AddAmount (WorldResources.Type.WOOD, unitInfo.resources.wood);
+			Player.getOwner (entity).resources.AddAmount (WorldResources.Type.METAL, unitInfo.resources.metal);
+			Player.getOwner (entity).resources.AddAmount (WorldResources.Type.FOOD, unitInfo.resources.food);
 		}
 	}
 	
