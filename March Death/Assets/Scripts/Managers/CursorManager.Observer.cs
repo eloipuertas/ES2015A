a50d1a76bcee4c10d40116f77bf134d0a6d61188
hermetico <yojmarinv@gmail.com>
2015-11-16 02:15:15 +0100

    Fixed another bug in cursor manager

--- March Death/Assets/Scripts/Managers/CursorManager.Observer.cs
+++ March Death/Assets/Scripts/Managers/CursorManager.Observer.cs
@@ -154,15 +154,19 @@ namespace Managers
         private bool ItsEnemy(GameObject _object)
         {
             IGameEntity entity = _object.GetComponent<IGameEntity>();
-            /// if it's dead is not an enemy
-            if (entity.info.race != _player.race 
+            if (entity != null)
+            {
+
+                /// if it's dead is not an enemy
+                if (entity.info.race != _player.race 
                 && entity.status != EntityStatus.DEAD 
                 && entity.status != EntityStatus.DESTROYED)
                 return true;
-            else
-                return false;
+                else
+                    return false;
+            }
+            else return false;
         }
-
 
 
     }
