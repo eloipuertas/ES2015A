e3fcef3033b8faac5f428092a166988f5cdd210a
c30zD <c30zD.x@gmail.com>
2015-11-22 01:10:32 +0100

    Moved human stronghold and hero to the left corner of the mini-map. Some trees have been removed from the main scene. Those trees belong to tree1_nature_human_mini_forest: * tree1_nature(8) * tree1_nature(5) * tree3_nature (46) * tree1_nature (6) * tree3_nature (23) * tree3_nature (17)

--- March Death/Assets/Scripts/Gameplay/GameInformation.cs
+++ March Death/Assets/Scripts/Gameplay/GameInformation.cs
@@ -126,13 +126,13 @@ public class GameInformation : MonoBehaviour {
         t.unit = UnitTypes.HERO;
         game.AddMission(Battle.MissionType.DESTROY, 1, EntityType.UNIT, t, 0, true, "");
         Battle.PlayerInformation player = new Battle.PlayerInformation(Races.MEN);
-        player.AddBuilding(BuildingTypes.STRONGHOLD, 801.4f, 753.6f);
-        player.AddUnit(UnitTypes.HERO, 801.4f, 785f);
+        player.AddBuilding(BuildingTypes.STRONGHOLD, 960, 832.5f);
+        player.AddUnit(UnitTypes.HERO, 937.5f, 819.64f);
         player.SetInitialResources(2000, 2000, 2000, 2000);
         game.AddPlayerInformation(player);
         player = new Battle.PlayerInformation(Races.ELVES);
-        player.AddUnit(UnitTypes.HERO, 946, 70);
-        player.AddBuilding(BuildingTypes.STRONGHOLD, 880, 70);
+        player.AddUnit(UnitTypes.HERO, 331.35f, 575.81f);
+        player.AddBuilding(BuildingTypes.STRONGHOLD, 283.7f, 562.5f);
         player.SetInitialResources(2000, 2000, 2000, 2000);
         game.AddPlayerInformation(player);
         game.SetWorldResources(5000, 5000, 5000);
