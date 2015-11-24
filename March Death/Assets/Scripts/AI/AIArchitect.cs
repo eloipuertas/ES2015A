0b276b5c20b1f5597796927d3a1faa3f3f468949
4nc3str4l <cmuri_10@hotmail.com>
2015-11-24 21:31:50 +0100

    Beguinning to read the file map

---
+++ March Death/Assets/Scripts/AI/AIArchitect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace Assets.Scripts.AI
+{
+    class AIArchitect
+    {
+        String currentBaseMap;
+
+        public AIArchitect(MacroManager ai)
+        {
+            readMap("easy_base_1.txt");
+        }
+
+        /// <summary>
+        /// Reads a file containing the map
+        /// </summary>
+        /// <param name="mapName"></param>
+        public void readMap(String mapName)
+        {
+            StreamReader sr = new StreamReader(Application.dataPath + "/AIBaseMaps/" + mapName);
+            currentBaseMap = sr.ReadToEnd();
+            sr.Close();
+
+            String[] lines = currentBaseMap.Split("\n"[0]);
+
+            foreach (String line in lines)
+            {
+                Debug.Log(line);
+            }
+        }
+    }
+}
