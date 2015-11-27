92bb96dc14bc3b56e868d90087edc8fb4797f589
4nc3str4l <cmuri_10@hotmail.com>
2015-11-27 10:12:17 +0100

    Trying to make something cool

--- March Death/Assets/Scripts/AI/AIArchitect.cs
+++ March Death/Assets/Scripts/AI/AIArchitect.cs
@@ -1,16 +1,30 @@
 ﻿using System;
-using System.IO;
 using UnityEngine;
 
 namespace Assets.Scripts.AI
 {
     class AIArchitect
     {
+        Color stronghold = new Color(0.000f, 0.000f, 0.059f, 1.000f);
+        Color militaryBuilding = new Color(0, 0, 0, 1.000f);
+        Color resourcesBuilding = new Color(0, 0, 0, 1.000f);
+
+        Color tower = new Color(0, 0, 0, 1.000f);
+        Color horizontallWall = new Color(0, 0, 0, 1.000f);
+        Color verticallWall = new Color(0, 0, 0, 1.000f);
+
+        Color cornerWall = new Color(0, 0, 0, 1.000f);
+        Color defenceZone = new Color(0, 0, 0, 1.000f);
+        Color emptySpace = new Color(0, 0, 0, 1.000f);
+
         String currentBaseMap;
 
-        public AIArchitect(MacroManager ai)
+        AIController ai;
+
+        public AIArchitect(AIController aiController)
         {
-            readMap("easy_base_1.txt");
+            readMap("pixels");
+            ai = aiController;
         }
 
         /// <summary>
@@ -19,16 +33,27 @@ namespace Assets.Scripts.AI
         /// <param name="mapName"></param>
         public void readMap(String mapName)
         {
-            StreamReader sr = new StreamReader(Application.dataPath + "/AIBaseMaps/" + mapName);
-            currentBaseMap = sr.ReadToEnd();
-            sr.Close();
+            Texture2D mapTexture = Resources.Load<Texture2D>("Data/AIBaseMaps/" + mapName);
+            Texture2D tex = new Texture2D(9, 1, TextureFormat.BGRA32, false);
+            byte[] fileData;
 
-            String[] lines = currentBaseMap.Split("\n"[0]);
-
-            foreach (String line in lines)
+            if (mapTexture == null)
             {
-                Debug.Log(line);
+                Debug.Log("AIArchitect: " + mapName + " could not be found");
+                return;
             }
+
+            Color[] pixels = mapTexture.GetPixels();
+
+            for (int i = 0; i < mapTexture.height; i++)
+            {
+                for(int j = 0; j < mapTexture.width; j++)
+                {
+                    Console.WriteLine(mapTexture.GetPixel(i, j));
+                }
+            }
+
+            Debug.Log("Finished Parsing Map");
         }
     }
 }
