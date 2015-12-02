9b3dd19a226b9ba5cab3cedd699d9da234840d38
MSoler <msolerpl10@alumnes.ub.edu>
2015-12-02 05:03:47 +0100

    [AI] Patrol closes #403 Explorer Takes arms AI units get assigned to multiple squads Fix bug with bAgent Fix multiple small bugs

--- March Death/Assets/Scripts/AI/Agents/ExplorerAgent.cs
+++ March Death/Assets/Scripts/AI/Agents/ExplorerAgent.cs
@@ -118,15 +118,6 @@ namespace Assets.Scripts.AI.Agents
 
             if (fowManager.Enabled)
             {
-                /*
-                TODO: takeArms Explorer
-                if (squad.units.Count < 2)
-                {
-                    int num = 2 - squad.Units.Count();
-                    if (ai.Macro.canTakeArms() >= num)
-                        ai.Macro.takeArms(num);
-                }
-                */
                 bool lostHero = (heroLastPos != Vector3.zero && !heroVisible && squad.Units.Count > 0);
 
                 // Static values
@@ -255,6 +246,14 @@ namespace Assets.Scripts.AI.Agents
             numberSquadsExploring = 0;
         }
 
+        public override void PostUpdate()
+        {
+            if (numberSquadsExploring < MaxExplorerSquads && numberSquadsExploring>0)
+            {
+                if (ai.Macro.canTakeArms() >= 2)
+                    ai.Macro.takeArms(2);
+            }
+        }
         static float angleDirection(Vector3 from, Vector3 initialTo, Vector3 newTo)
         {
             Vector3 referenceForward = (initialTo - from).normalized;
@@ -366,8 +365,7 @@ namespace Assets.Scripts.AI.Agents
 			{
 				confidence += CONFIDENCE_EXPLORER_ALL_CIVILS;
 			}
-
-
+            
             if (numberSquadsExploring > MaxExplorerSquads)
             {
                 confidence += CONFIDENCE_TOO_MANY_EXPLORING;
--- March Death/Assets/Scripts/Entities/Squad.cs
+++ March Death/Assets/Scripts/Entities/Squad.cs
@@ -59,6 +59,17 @@ public sealed class Squad : BareObserver<Squad.Actions>
             return ((SmelledEnemies)_data[DataType.SMELLED_ENEMIES]).Value;
         }
     }
+    public int PatrolPosition
+    {
+        get
+        {
+            return ((PatrolData)_data[DataType.PATROL_DATA]).PatrolPosition;
+        }
+        set
+        {
+            ((PatrolData)_data[DataType.PATROL_DATA]).PatrolPosition = value;
+        }
+    }
 
     private Storage.Races _race;
     public Storage.Races Race
@@ -87,6 +98,7 @@ public sealed class Squad : BareObserver<Squad.Actions>
 
         _data.Add(DataType.BOUNDING_BOX, new BoundingBox(this));
         _data.Add(DataType.ATTACK_VALUE, new AttackValue(this));
+        _data.Add(DataType.PATROL_DATA, new PatrolData(this));
 
         // Smelled squads can not smell enemies or we would run into endless recursion
         if (smellEnemies)
@@ -288,7 +300,7 @@ public sealed class SquadUpdater : GameEntity<SquadUpdater.DummyActions>
     }
 }
 
-public enum DataType { BOUNDING_BOX, ATTACK_VALUE, SMELLED_ENEMIES }
+public enum DataType { BOUNDING_BOX, ATTACK_VALUE, SMELLED_ENEMIES, PATROL_DATA }
 
 public interface ISquadData
 {
@@ -424,5 +436,18 @@ public class SmelledEnemies : SquadData<Squad>
             _enemySquad.UpdateUnits(enemyUnits);
             _enemySquad.Update();
         }
+    }
+}
+
+public class PatrolData : SquadData<int>
+{
+    public int PatrolPosition { get; set; }
+    public override int Value { get { return PatrolPosition; } }
+    public PatrolData(Squad squad) : base(squad) {
+        PatrolPosition = 0;
+    }
+    public override void Update(List<Unit> units)
+    {
+        //updated externally
     }
 }
