c3bcc382d88303023fa19028bb9e3fda9b3850ae
rperezde11 <raulp_prat@hotmail.com>
2015-12-06 21:41:23 +0100

    more changes :/

---
+++ March Death/Assets/Scripts/Events/ResourcesEvents.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Utils;
+using System.Collections;
+
+public class ResourcesEvents : Singleton<ResourcesEvents>
+{
+
+    private ResourcesEvents()
+    {
+
+    }
+
+    public void registerToEvents(IGameEntity entity)
+    {
+        if (entity.info.isResource)
+        {
+            Resource resource = (Resource)entity;
+            resource.register(Resource.Actions.NEW_HARVEST, OnNewHarvest);
+            resource.register(Resource.Actions.NEW_EXPLORER, OnNewExplorer);
+        }
+    }
+
+    private void OnNewHarvest(System.Object obj)
+    {
+        PopulationInfo.get.AddWorker();
+    }
+
+    private void OnNewExplorer(System.Object obj)
+    {
+        PopulationInfo.get.RemoveWorker();
+    }
+}
