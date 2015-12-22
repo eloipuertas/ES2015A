ab98b25296dee97b3494f19fb0d9053481f0c4d9
MSoler <msolerpl10@alumnes.ub.edu>
2015-12-22 02:36:22 +0100

    Default difficulty should be 1 no 0

--- March Death/Assets/Scripts/Gameplay/GameInformation.cs
+++ March Death/Assets/Scripts/Gameplay/GameInformation.cs
@@ -33,7 +33,7 @@ public class GameInformation : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        Difficulty = 1;
     }
 
     // Update is called once per frame
